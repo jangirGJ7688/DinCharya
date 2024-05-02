6b05fea9c211fd02488d7929a8a4ebb456f69210
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2024-05-02 12:55:15 +0530

    Initial Commit

---
+++ DinCharya/DinCharyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinCharyaApp.swift
+//  DinCharya
+//
+//  Created by Ganpat Jangir on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DinCharyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
